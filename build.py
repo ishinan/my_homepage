7df11bead33dc8bc1db63c5e5177d0a2f73212c3
Masaaki Yana <nativeyana@gmail.com>
2019-11-03 21:20:52 -0800

    build.py:  build_html_files_04(content_dir, target_dir)

--- build.py
+++ build.py
@@ -107,6 +107,39 @@ def build_html_files_03():
                 f_target.writelines(full_contents)
 
 
+def build_html_files_04(content_dir, target_dir):
+    '''
+    This function opens the top and bottom files once
+    This function opens the target file once
+    '''
+    part_top = [] 
+    part_bottom = []
+    # Read from top.html
+    with open('templates/top.html', 'r') as f:
+        part_top = f.readlines()
+    # Read from bottom.html
+    with open('templates/bottom.html', 'r') as f:
+        part_bottom = f.readlines()
+
+    # Make a list of content files
+    for curr, dirs, list_content_files in os.walk(content_dir):
+        #print(list_content_files)
+        for content_file in list_content_files:
+            
+            # Creating paths to contents html and target html
+            content_path = os.path.join(curr, content_file)
+            target_path = os.path.join(target_dir, content_file)
+
+            # Build the contents of html
+            with open(content_path , 'r') as f_content:
+                contents = f_content.readlines()
+            full_contents = part_top + contents + part_bottom
+
+            # Write to a target file once
+            with open(target_path, 'w') as f_target:
+                f_target.writelines(full_contents)
+
+
 if __name__ == "__main__":
-    build_html_files_03()
+    build_html_files_04("contents", "docs")
         

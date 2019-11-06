bd1c6c6e262d3b45975596f519e3baca3719156c
Masaaki Yana <nativeyana@gmail.com>
2019-11-05 21:37:29 -0800

    build.py: Added comments and cleaned empty lines

--- build.py
+++ build.py
@@ -2,11 +2,12 @@
 """
 Version: 
 Feature:
-    Read Templates/top.html and bottom.html and Contents html files, and create html files
+    static site generator
+    Read template html files and contents html files, and create html files
 
 Regquirements
-    - templates/top.html, templates/bottom.html and contents/*.html
-    - docs directory
+    - templates file(s) and contents or contents02
+    - docs directory as destination
 
 Change Log:
     - N/A
@@ -22,16 +23,17 @@ import logging.config
 from string import Template
 
 
-
 # Read log.cfg file for loggeing congiguration
 logging.config.fileConfig('log.cfg')
 
 def build_html_files_one_template(content_dir, target_dir):
     '''
-    This function read one template html
-    Relapace the contents and title and nav's active by tempalate module
-    This function opens the target file once
+    This function reads one template html
+    Relapace the contents and title by tempalate module
+    Add "active" css class for nav by string.replace()
+    Write the content to the target file
     '''
+    # Dict key is html file name, value is string for title tag
     title_data = {
         'index': 'Home',
         'blog': 'Blog',
@@ -46,26 +48,23 @@ def build_html_files_one_template(content_dir, target_dir):
 
     # Make a list of content files
     for curr, dirs, list_content_files in os.walk(content_dir):
-        #print(list_content_files)
         for content_file in list_content_files:
             # Creating paths to contents html and target html
             content_path = os.path.join(curr, content_file)
             target_path = os.path.join(target_dir, content_file)
             html_name = content_file.replace(".html", "")
 
-            # Build the contents of html
+            # Read a content html
             with open(content_path , 'r') as f_content:
                 content = f_content.read()
 
-            # print(type(content))
-            # print(title_data[html_name])
-            # print(template)
-
+            # Build the full content by template 
             full_contents = template.safe_substitute(
                    title = title_data[html_name],
                    html_content = content
                 )
 
+            # Add "active" css class for nav
             full_contents = full_contents.replace(f"\" href=\"./{html_name}", f" active\" href=\"./{html_name}")
 
             # Write to a target file once
@@ -75,8 +74,10 @@ def build_html_files_one_template(content_dir, target_dir):
 
 def build_html_files(content_dir, target_dir):
     '''
-    This function read the top and bottom files once
-    This function write the target file once
+    1. Read the top and bottom files once
+    2. Read a content file
+    3. Build a full content 
+    4. Write the full content to a file
     '''
     part_top = [] 
     part_bottom = []

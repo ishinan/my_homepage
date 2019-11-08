651d8072c359071c4ec24c3221051dc58658cd2a
Masaaki Yana <nativeyana@gmail.com>
2019-11-07 19:05:51 -0800

    build.py: Removing function using top/bottom htmls, and renaming to base.html, and correcting content path

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ Feature:
     Read template html files and contents html files, and create html files
 
 Regquirements
-    - templates file(s) and contents or contents02
+    - base template html and content html files
     - docs directory as destination
 
 Change Log:
@@ -26,7 +26,7 @@ from string import Template
 # Read log.cfg file for loggeing congiguration
 logging.config.fileConfig('log.cfg')
 
-def build_html_files_one_template(content_dir, target_dir):
+def build_html_files(content_dir, target_dir):
     '''
     This function reads one template html
     Relapace the contents and title by tempalate module
@@ -46,7 +46,7 @@ def build_html_files_one_template(content_dir, target_dir):
     logger.debug(f"title_data: {title_data}")
 
     # Read from one_template.html
-    with open('templates/one_template.html', 'r') as f:
+    with open('templates/base.html', 'r') as f:
         template_html  = f.read()
     template = Template(template_html)
 
@@ -79,42 +79,5 @@ def build_html_files_one_template(content_dir, target_dir):
                 logger.info(f"wrote the {html_name} to output file: {target_path}")
 
 
-
-def build_html_files(content_dir, target_dir):
-    '''
-    1. Read the top and bottom files once
-    2. Read a content file
-    3. Build a full content 
-    4. Write the full content to a file
-    '''
-    part_top = [] 
-    part_bottom = []
-    # Read from top.html
-    with open('templates/top.html', 'r') as f:
-        part_top = f.readlines()
-    # Read from bottom.html
-    with open('templates/bottom.html', 'r') as f:
-        part_bottom = f.readlines()
-
-    # Make a list of content files
-    for curr, dirs, list_content_files in os.walk(content_dir):
-        #print(list_content_files)
-        for content_file in list_content_files:
-            
-            # Creating paths to contents html and target html
-            content_path = os.path.join(curr, content_file)
-            target_path = os.path.join(target_dir, content_file)
-
-            # Build the contents of html
-            with open(content_path , 'r') as f_content:
-                contents = f_content.readlines()
-            full_contents = part_top + contents + part_bottom
-
-            # Write to a target file once
-            with open(target_path, 'w') as f_target:
-                f_target.writelines(full_contents)
-
-
 if __name__ == "__main__":
-    #build_html_files("contents", "docs")
-    build_html_files_one_template("contents02", "docs")
+    build_html_files("content", "docs")

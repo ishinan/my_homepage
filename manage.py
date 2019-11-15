8ab51ddc12e2d37c6cba6c4eaae354171f6ecadc
Masaaki Yana <nativeyana@gmail.com>
2019-11-15 12:44:15 -0800

    manage.py: added if condition to number of arguments

--- manage.py
+++ manage.py
@@ -12,21 +12,30 @@ def usage():
     {utility_name} build : To create static html files based on templates and contents
     """)
 
+def main_test():
+    '''
+    Run utils with argements(new or build). 
+    '''
+    # Removing python command if this utility is called with a python command
+    python_cmd_list = [ 'python', 'python2', 'python3', 'py2', 'py3' ]
+    if sys.argv[0] in python_cmd_list:
+        sys.argv = sys.argv[1:]
+
+    # Note sys.argv includes the command itself;e.g. 'manage.py new' is two elements
+    if len(sys.argv) == 2:
+        print("one arg")
+    else:
+        print("Must have one argument [new|build]")
+        usage()
+
 def main():
     '''
-    Run utils with argements
-    Allowed arguements
+    Run utils with argements(new or build). 
     '''
-
-    # If argements is zero, just run simple
-    # If argement is more than one, print error
-    # If argement is one, and then new or build, run the task
     # This create main html files
     utils.build_html_files_from_base()
     # This create blog html files
     utils.build_blog_html_files_from_blog_base()
 
-
 if __name__ == '__main__':
-    #main()
-    usage()
+    main_test()

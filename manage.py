90864d33a3dc6dc105c44c79389acd687cb1ff65
Masaaki Yana <nativeyana@gmail.com>
2019-11-15 12:27:04 -0800

    manage.py: added usage() function to print usage of this utility

--- manage.py
+++ manage.py
@@ -1,7 +1,32 @@
+import sys
+import os
 import utils
 
-if __name__ == '__main__':
+def usage():
+    '''
+    print usage if arguments is not proper
+    '''
+    utility_name = os.path.basename(sys.argv[0])
+    print(f"""Usage:
+    {utility_name} new   : To create a new content html file
+    {utility_name} build : To create static html files based on templates and contents
+    """)
+
+def main():
+    '''
+    Run utils with argements
+    Allowed arguements
+    '''
+
+    # If argements is zero, just run simple
+    # If argement is more than one, print error
+    # If argement is one, and then new or build, run the task
     # This create main html files
     utils.build_html_files_from_base()
     # This create blog html files
     utils.build_blog_html_files_from_blog_base()
+
+
+if __name__ == '__main__':
+    #main()
+    usage()

ef2c142c70c6b3288682881591f6a740c1b27f77
Masaaki Yana <nativeyana@gmail.com>
2019-11-15 12:08:00 -0800

    hw04: Phase4 Splitting into utils.py and manage.py

---
+++ manage.py
@@ -0,0 +1,7 @@
+import utils
+
+if __name__ == '__main__':
+    # This create main html files
+    utils.build_html_files_from_base()
+    # This create blog html files
+    utils.build_blog_html_files_from_blog_base()

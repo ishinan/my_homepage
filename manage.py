07f762d85f44c33b3f19e0ba458ff3a234a5dd43
Masaaki Yana <nativeyana@gmail.com>
2019-11-17 17:29:37 -0800

    manage.py: added build_blog_page() to test building a new blog page

--- manage.py
+++ manage.py
@@ -19,7 +19,7 @@ def build_static_site():
     # This create main html files
     utils.build_html_files_from_base()
     # This create blog html files
-    utils.build_blog_html_files_from_blog_base()
+    #utils.build_blog_html_files_from_blog_base()
 
 def ask_contents():
     '''
@@ -39,18 +39,24 @@ def ask_contents():
 def create_new_content_file():
     '''
     create a new content file in 'content' directory
-
     '''
     content_dict_user_input = ask_contents()
     utils.create_new_content_file(content_dict=content_dict_user_input)
 
-def main_test():
+def build_blog_page():
+    '''
+    Testing to build a blog page 
+    '''
+    utils.build_blog_html_files_from_blog_base02()
+
+def main():
     '''
     Run utils with argements(new or build). 
     '''
     func_list = {
         'new': create_new_content_file,
         'build': build_static_site,
+        'bblog': build_blog_page,
     }
     # Removing python command if this utility is called with a python command
     python_cmd_list = [ 'python', 'python2', 'python3', 'py2', 'py3' ]
@@ -64,7 +70,7 @@ def main_test():
         print("Must have one argument [new|build]")
         usage()
 
-def main():
+def main_old():
     '''
     Run utils with argements(new or build). 
     '''
@@ -73,5 +79,6 @@ def main():
     # This create blog html files
     utils.build_blog_html_files_from_blog_base()
 
+
 if __name__ == '__main__':
-    main_test()
+    main()
